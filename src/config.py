16dbab0a82c6f4b5cfc00f045f45ceccc9788256
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-03-20 15:01:54 +0600

    fix data import

--- src/config.py
+++ src/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     NEO4J_URI: str = "bolt://localhost:7687"
     NEO4J_USERNAME: str = "neo4j"
     NEO4J_PASSWORD: str
+    NEO4J_DATABASE: str = "neo4j"
     LLM_API_KEY: str
     LLM_NAME: str = "groq"
     LLM_API_ENDPOINT: str = "https://api.example.com"
