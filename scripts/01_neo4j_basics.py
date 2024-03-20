3be0011ebb3360fb75f2ac4ecb19e91625e66500
Shamsuddin Ahmed <shamsuddinpias0@gmail.com>
2024-03-20 15:03:31 +0600

    add basic lecture with cypher

--- scripts/01_neo4j_basics.py
+++ scripts/01_neo4j_basics.py
@@ -9,3 +9,15 @@ NEO4J_URI = settings.NEO4J_URI
 NEO4J_USERNAME = settings.NEO4J_USERNAME
 NEO4J_PASSWORD = settings.NEO4J_PASSWORD
 NEO4J_DATABASE = settings.NEO4J_DATABASE
+
+kg = Neo4jGraph(
+    url=NEO4J_URI, username=NEO4J_USERNAME, password=NEO4J_PASSWORD, database=NEO4J_DATABASE
+)
+
+cypher = """
+  MATCH (n) 
+  RETURN count(n)
+  """
+
+result = kg.query(cypher)
+print(result)
